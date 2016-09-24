85a63ebbd97c4776697e1a753bcb091b417adefc
Samuel Ytterbrink <neppord@gmail.com>
2016-09-24 20:47:40 +0200

    Implement bounce with spacebar.

--- src/ball/Handler.java
+++ src/ball/Handler.java
@@ -8,10 +8,10 @@ public class Handler {
 
     private final World world;
 
-    private final Point2D upTug = new Point2D(0, 0.1);
-    private final Point2D downTug = new Point2D(0, -0.1);
-    private final Point2D leftTug = new Point2D(-0.1, 0);
-    private final Point2D rightTug = new Point2D(0.1, 0);
+    private final Point2D upTug = new Point2D(0, 1);
+    private final Point2D downTug = new Point2D(0, -1);
+    private final Point2D leftTug = new Point2D(-1, 0);
+    private final Point2D rightTug = new Point2D(1, 0);
 
     public Handler(World world) {
         this.world = world;
@@ -20,9 +20,10 @@ public class Handler {
     public void handleKeyPressed(KeyEvent event) {
         switch (event.getCode()) {
             case D:
-            case RIGHT: world.ioTug = rightTug; break;
+            case RIGHT: world.ioTug = rightTug.multiply(0.1); break;
             case A:
-            case LEFT: world.ioTug = leftTug; break;
+            case LEFT: world.ioTug = leftTug.multiply(0.1); break;
+            case SPACE: world.ioTug = downTug; break;
         }
     }
 
